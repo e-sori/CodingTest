bec318048227d257d5ab86f6e58379b980a6b5a9
이소리 <138553313+e-sori@users.noreply.github.com>
2023-08-30 22:33:54 +0900

    [Bronze IV] Title: 주사위 세개, Time: 208 ms, Memory: 17708 KB -BaekjoonHub

--- 백준/Bronze/2480. 주사위 세개/주사위 세개.java
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.java
@@ -6,27 +6,21 @@ public class Main {
     public static void main(String args[]) {
         Scanner scan = new Scanner(System.in);
 
-        int num1 = scan.nextInt();
-        int num2 = scan.nextInt();
-        int num3 = scan.nextInt();
+        int A = scan.nextInt();
+        int B = scan.nextInt();
+        int C = scan.nextInt();
+        
+        int print;
 
-        // 배열 : 길이가 정해져 있을 때
-        // List & ArrayList : 길이를 모를 때
-        int[] arrNum = new int[] {num1,num2,num3};
-        List<Integer> selectNum = new ArrayList<Integer>();
-        int maxNum = arrNum[0];
-
-        for(int i = 0; i < arrNum.length; i += 1){
-            for(int j = i+1; j < arrNum.length; j += 1){
-                if(arrNum[i] == arrNum[j]) selectNum.add(arrNum[i]);
-                else if(maxNum < arrNum[j]) {
-                    maxNum = arrNum[j];
-                }
-            }
+        if(A == B){
+            if(B == C) {
+                print = 10000 + A * 1000;
+            }else print = 1000 + A * 100;
+        }else if(B == C || A == C) {
+            print = 1000 + C * 100;
+        }else {
+            print = Math.max(Math.max(A,B),C) * 100;
         }
-
-        if (selectNum.size() == 3) System.out.println(10000 + selectNum.get(0) * 1000);
-        else if (selectNum.size() == 1) System.out.println(1000 + selectNum.get(0) * 100);
-        else if (selectNum.size() == 0) System.out.println(maxNum * 100);
+        System.out.println(print);
     }
 }
