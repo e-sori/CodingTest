1ea013d4799e2b968d79bddcd2fd9825cdb53771
이소리 <138553313+e-sori@users.noreply.github.com>
2023-09-05 21:13:25 +0900

    [Bronze V] Title: 아스키 코드, Time: 132 ms, Memory: 14280 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+
+public class Main{
+    public static void main(String[] args) throws IOException{
+    	BufferedReader br = new BufferedReader (new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter (new OutputStreamWriter(System.out));
+        
+        String str = br.readLine();
+        
+        int print = str.charAt(0);
+        
+        bw.write(String.valueOf(print));
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
