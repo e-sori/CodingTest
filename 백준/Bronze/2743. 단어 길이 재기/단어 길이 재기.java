abf190ec11995a21bfa4ba8bac8c9020bde16c8a
이소리 <138553313+e-sori@users.noreply.github.com>
2023-09-05 19:54:33 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 124 ms, Memory: 14264 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+
+public class Main{
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        String str = br.readLine();
+        
+        bw.write(String.valueOf(str.length()));
+        
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
